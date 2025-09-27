4603f7d7096916fc8982f7a9b3e54c2551b19a21
Roland Neuber <rolandmartinneuber@gmail.com>
2025-09-27 17:40:22 +0200

    Separate lexing and parsing into separate modules.

---
+++ lexing.py
@@ -0,0 +1,48 @@
+"""This module is responsible for tokenizing the source code string into a list of tokens."""
+
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class Token:
+    """Base class for all tokens emitted by the tokenizer."""
+
+
+@dataclass
+class Plus(Token):
+    """Token for the '+' operator."""
+
+
+@dataclass
+class Star(Token):
+    """Token for the '*' operator."""
+
+
+@dataclass
+class Number(Token):
+    """Number literal, both token and expression."""
+    value: int
+
+
+def tokenize(source: str) -> List[Token]:
+    """Tokenizes the source code string into a list of tokens."""
+    acc = ""
+    result: List[Token] = []
+
+    while len(source) != 0:
+        acc += source[0]
+        source = source[1:]
+        match acc:
+            case " ":
+                pass
+            case "+":
+                result.append(Plus())
+            case "*":
+                result.append(Star())
+            case _:
+                if len(source) != 0 and source[0].isdigit():
+                    continue # Not all digits of the number have been yielded.
+                result.append(Number(int(acc)))
+        acc = ""
+    return result
